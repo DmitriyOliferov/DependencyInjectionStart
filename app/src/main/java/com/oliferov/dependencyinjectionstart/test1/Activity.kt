de92b13e820a9b27aa7fabf3f1a63db35bc35498
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-04-06 13:45:56 +0300

    add dagger in gradle

--- app/src/main/java/com/oliferov/dependencyinjectionstart/test1/Activity.kt
+++ app/src/main/java/com/oliferov/dependencyinjectionstart/test1/Activity.kt
@@ -2,7 +2,7 @@ package com.oliferov.dependencyinjectionstart.test1
 
 class Activity {
 
-    lateinit var computer: Computer
+
     lateinit var keyboard: Keyboard
 
     init {
--- app/src/main/java/com/oliferov/dependencyinjectionstart/test2/MainActivity.kt
+++ app/src/main/java/com/oliferov/dependencyinjectionstart/test2/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.oliferov.dependencyinjectionstart.test1
+package com.oliferov.dependencyinjectionstart.test2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oliferov.dependencyinjectionstart.R
+import com.oliferov.dependencyinjectionstart.test1.Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val activity = Activity()
+        activity.keyboard.toString()
     }
 }
